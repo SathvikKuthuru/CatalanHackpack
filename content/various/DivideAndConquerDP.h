a157b5d97dc2fea75dd785f72b52723e223aee6a
Sathvik Kuthuru <31018656+SathvikKuthuru@users.noreply.github.com>
2024-02-16 18:06:56 -0500

    Update DivideAndConquerDP.h

--- content/various/DivideAndConquerDP.h
+++ content/various/DivideAndConquerDP.h
@@ -1,5 +1,5 @@
 /**
- * Author: Simon Lindholm
+ * Author: Simon Lindholm, Sathvik Kuthuru
  * License: CC0
  * Source: Codeforces
  * Description: Given $a[i] = \min_{lo(i) \le k < hi(i)}(f(i, k))$ where the (minimal)
@@ -27,3 +27,51 @@ struct DP { // Modify at will:
 	}
 	void solve(int L, int R) { rec(L, R, INT_MIN, INT_MAX); }
 };
+
+// Alternate Implementation Example: Modify at will
+const int MAXN = 3000;
+ 
+vl dp_before(MAXN), dp_cur(MAXN);
+vl pre(MAXN);
+int m, n;
+ 
+ll C(int i, int j) {
+    ll ret = pre[j];
+    if(i) ret -= pre[i - 1];
+    return ret * ret;
+}
+ 
+void compute(int l, int r, int optl, int optr) {
+    if (l > r)
+        return;
+ 
+    int mid = (l + r) >> 1;
+    pair<long long, int> best = {LLONG_MAX, -1};
+ 
+    for (int k = optl; k <= min(mid, optr); k++) {
+        best = min(best, {(k ? dp_before[k - 1] : 0) + C(k, mid), k});
+    }
+ 
+    dp_cur[mid] = best.first;
+    int opt = best.second;
+ 
+    compute(l, mid - 1, optl, opt);
+    compute(mid + 1, r, opt, optr);
+}
+ 
+void solve() {
+    cin >> n >> m;
+ 
+    for(int i = 0; i < n; i++) {
+        cin >> pre[i];
+        if(i) pre[i] += pre[i - 1];
+        dp_before[i] = C(0, i);
+    }
+ 
+    for(int i = 1; i < m; i++) {
+        compute(0, n - 1, 0, n - 1);
+        swap(dp_before, dp_cur);
+    }
+ 
+    cout << dp_before[n - 1] << '\n';
+}
