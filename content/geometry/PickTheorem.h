cd6e7f079aba63a9f15b95751fd186b630216e45
Sathvik Kuthuru <31018656+SathvikKuthuru@users.noreply.github.com>
2024-02-16 22:36:09 -0500

    Create PickTheorem.h

---
+++ content/geometry/PickTheorem.h
@@ -0,0 +1,9 @@
+/**
+ * Source: cp-algo
+ * Description: Given a certain lattice polygon with non-zero area.
+ * We denote its area by  $S$ , the number of points with integer coordinates lying 
+ * strictly inside the polygon by  $I$  and the number of points lying on 
+ * polygon sides by $B$ .
+ * Then, the Pick's formula states:
+ * $$S=I+\frac{B}{2}-1$$ 
+ */
